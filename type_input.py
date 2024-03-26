23eb9c5bfefec889841e8457f6c6d0a9f90d312b
Lito Frito <56422761+Lito-Frito@users.noreply.github.com>
2024-03-26 15:46:13 -0400

    Modularize text prompts

--- type_input.py
+++ type_input.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 def find_types():
     """This function determines the pokemon's type(s) and return them to the main function"""
     
@@ -5,15 +7,18 @@ def find_types():
     acceptable_types = ["normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison", "ground",
                         "flying", "psychic", "bug", "rock", "ghost", "dragon", "dark", "steel", "fairy"]
     
+    prompt = f"""\n
+                You entered: {{pokemon_type}}
+                Here is a list of all acceptable types: {acceptable_types}
+                Please enter a valid type."""
+    
     # Ask for first type and ensure type is valid:
     while True:
         pokemon_type1 = input("\nWhat is the pokemon's first type? > ").lower()
         if pokemon_type1 in acceptable_types:
             break
         else:
-            print(f"\nHere is a list of all acceptable types: {acceptable_types}")
-            print(f"\nYou entered: {pokemon_type1}")
-            print("\nPlease enter a valid type.")
+            print(dedent(prompt.format(pokemon_type=pokemon_type1)))
   
     # Ask for second type and ensure type is valid (in acceptable_types AND not the same as pokemon_type1)
     # If the second type is "none", then the pokemon only has one type and the program will continue:
@@ -24,12 +29,11 @@ def find_types():
         elif pokemon_type2 == "none":
             break
         else:
-            print(f"\nHere is a list of all acceptable types: {acceptable_types}")
-            print(f"\nYou entered: {pokemon_type2}")
-            
             # Double check that the user didn't enter the same type as the first type:
             if pokemon_type2 == pokemon_type1:
-                print("\nYou entered the same type as the first type.") 
-            print("\nPlease enter a valid type or 'none'.")
+                print("\nYou entered the same type as the first type!" + dedent(prompt.format(pokemon_type=pokemon_type2)))
+                # print(dedent(prompt.format(pokemon_type=pokemon_type2)))
+            else:
+                print(dedent(prompt.format(pokemon_type=pokemon_type2)))
     
     return pokemon_type1, pokemon_type2
